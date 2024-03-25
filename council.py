2dc4d7922830f0de0e5686ec67c789b19bafbe50
vachagunn <vachikpuper@gmail.com>
2024-03-25 22:52:15 +0500

    Скачивание pdf файлов

--- council.py
+++ council.py
@@ -1,8 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 import re
 
-# Сохранение ссылок аннотаций
+PAGE_COUNT = 3
+
+folder_path = 'downloads'
+
+# Сбор ссылок аннотаций
 def save_annotations(page_soup):
     annotation_links = []    
 
@@ -17,11 +22,30 @@ def save_annotations(page_soup):
     return annotation_links
 
 
-for page_number in range(5):
+# Скачивание pdf файлов
+def save_pdf(links):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    for link in links:
+        file_name = link.split('/')[-1]
+        file_path = os.path.join(folder_path, file_name)
+        print(file_path)
+
+        response = requests.get(link)
+        print(response)
+        print(response.content)
+
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+
+
+for page_number in range(PAGE_COUNT):
     url = f'http://council.gov.ru/activity/meetings/page/{page_number + 1}/'
     print('\nURL: ', url + '\n')
     
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'lxml')
 
-    save_annotations(soup)
+    links = save_annotations(soup)
+    save_pdf(links)
